af05d6c6dbffe26b8af34c1566f2ceb1ba44da71
lamd0806 <lamd9126@hotmail.com>
2021-11-22 20:53:25 -0500

    se agrega ruta de los demas componentes

--- src/app/modulos/estaciones/estaciones-routing.module.ts
+++ src/app/modulos/estaciones/estaciones-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { GetComponent } from './get/get.component';
 
 const routes: Routes = [
-  
+  {
+    path: 'create',
+    component: CreateComponent,
+  },
+  {
+    path: "edit",
+    component: EditComponent,
+  },
+  {
+    path: "get",
+    component: GetComponent,
+  }
 ];
 
 @NgModule({
--- src/app/modulos/rutas/rutas-routing.module.ts
+++ src/app/modulos/rutas/rutas-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { GetComponent } from './get/get.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'create',
+    component: CreateComponent,
+  },
+  {
+    path: "edit",
+    component: EditComponent,
+  },
+  {
+    path: "get",
+    component: GetComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
